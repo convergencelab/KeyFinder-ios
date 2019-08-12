5517f7edee276ace58189d9f69beab11bd03ae0b
Travis MacDonald <x2015khn@stfx.ca>
2019-08-12 11:46:39 -0300

    VoicingTemplate class completed

---
+++ KeyFinder/VoicingTemplate.swift
@@ -0,0 +1,40 @@
+//
+//  VoicingTemplate.swift
+//  KeyFinder
+//
+//  Created by Travis MacDonald on 2019-08-12.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
+
+public class VoicingTemplate {
+    
+    public let name: String
+    
+    public let bassTones: [BassTone]
+    
+    public let chordTones: [ChordTone]
+    
+    public init(name: String, bassIxs: [Int], chordIxs: [Int]) {
+        self.name = name
+        self.bassTones = VoicingTemplate.toBassTones(ixs: bassIxs)
+        self.chordTones = VoicingTemplate.toChordTones(ixs: chordIxs)
+    }
+    
+    public class func toBassTones(ixs: [Int]) -> [BassTone] {
+        var tones = [BassTone]()
+        for ix in ixs {
+            tones.append(BassTone(degree: ix))
+        }
+        return tones
+    }
+    
+    public class func toChordTones(ixs: [Int]) -> [ChordTone] {
+        var tones = [ChordTone]()
+        for ix in ixs {
+            tones.append(ChordTone(degree: ix))
+        }
+        return tones
+    }
+}
