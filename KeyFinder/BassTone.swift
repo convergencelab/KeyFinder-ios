d42d2dc3a3fe9581456489fcc3e20a638bc139c5
Travis MacDonald <x2015khn@stfx.ca>
2019-08-12 11:10:12 -0300

    Added tone classes

---
+++ KeyFinder/BassTone.swift
@@ -0,0 +1,19 @@
+//
+//  BassTone.swift
+//  KeyFinder
+//
+//  Created by Travis MacDonald on 2019-08-12.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
+
+public class BassTone: Tone {
+    
+    public private(set) var degree: Int
+    
+    public init(degree: Int) {
+        self.degree = degree
+    }
+    
+}
---
+++ KeyFinder/Tone.swift
@@ -0,0 +1,15 @@
+//
+//  Tone.swift
+//  KeyFinder
+//
+//  Created by Travis MacDonald on 2019-08-12.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
+
+public protocol Tone {
+    
+    var degree: Int { get }
+    
+}
