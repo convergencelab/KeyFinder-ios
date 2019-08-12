be3b22778759fb695cbb472f9b63e565c0b663d5
Travis MacDonald <x2015khn@stfx.ca>
2019-08-12 12:14:17 -0300

    Added equatable in tone

--- KeyFinder/BassTone.swift
+++ KeyFinder/BassTone.swift
@@ -8,12 +8,16 @@
 
 import Foundation
 
-public class BassTone: Tone {
+public class BassTone: Tone, Equatable {
     
     public private(set) var degree: Int
     
     public init(degree: Int) {
         self.degree = degree
+    }
+  
+    public static func == (lhs: BassTone, rhs: BassTone) -> Bool {
+        return lhs.degree == rhs.degree
     }
     
 }
--- KeyFinder/ChordTone.swift
+++ KeyFinder/ChordTone.swift
@@ -8,12 +8,16 @@
 
 import Foundation
 
-public class ChordTone: Tone {
+public class ChordTone: Tone, Equatable {
     
     public private(set) var degree: Int
     
     public init(degree: Int) {
         self.degree = degree
+    }
+    
+    public static func == (lhs: ChordTone, rhs: ChordTone) -> Bool {
+        return lhs.degree == rhs.degree
     }
     
 }
